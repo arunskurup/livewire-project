2fe41751030b5f2490b202bccc659d1907da2c89
arunskurup <arunskurup81@gmail.com>
2022-07-17 12:50:15 +0530

    calculator1

--- app/Http/Livewire/Calculator.php
+++ app/Http/Livewire/Calculator.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Calculator extends Component
 {
+    public $number1=0;
+    public $number2=0;
+    public string $action ='+';
+    public float $result=0;
+    public bool $desable=false;
     public function render()
     {
         return view('livewire.calculator');
