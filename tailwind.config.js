251c260096a2e3a8534617f9a2900e8c146e03ef
arunskurup <arunskurup81@gmail.com>
2022-07-17 12:12:12 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ccontent: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
