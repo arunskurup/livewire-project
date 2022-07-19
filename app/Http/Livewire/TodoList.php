f82e34432d69d96712e417058f403e32e6ab427e
arunskurup <arunskurup81@gmail.com>
2022-07-19 20:43:53 +0530

    todo list model call

--- app/Http/Livewire/TodoList.php
+++ app/Http/Livewire/TodoList.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\todo;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    public $todo;
+    public string $todolist;
+    public function mount(){
+        $this->selectTodos();
+    }
     public function render()
     {
         return view('livewire.todo-list');
