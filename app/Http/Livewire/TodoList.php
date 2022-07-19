0084169847d487cc696553ca2fb0696b7dcb0128
arunskurup <arunskurup81@gmail.com>
2022-07-19 20:52:30 +0530

    todo list data

--- app/Http/Livewire/TodoList.php
+++ app/Http/Livewire/TodoList.php
@@ -6,13 +6,24 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
-    public $todo;
-    public string $todolist;
+    public $todos;
+    public string $todoText;
     public function mount(){
         $this->selectTodos();
     }
     public function render()
     {
         return view('livewire.todo-list');
+    }
+    public function selectTodos(){
+        $this->todos=todo::orderBy('created_at','DESC')->get();
+    }
+    public function addTodos(){
+        $todo= new todo;
+        $todo->todo=$this->todoText;
+        $todo->completed= false;
+        $todo->save();
+        $this->todoText='';
+        $this->setTodos();
     }
 }
