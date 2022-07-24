4d951dce984aadba374ebb13cf9d295356dc384f
arunskurup <arunskurup81@gmail.com>
2022-07-24 14:34:10 +0530

    cascide design1

--- resources/views/livewire/cascading-dropdown.blade.php
+++ resources/views/livewire/cascading-dropdown.blade.php
@@ -1,3 +1,18 @@
 <div>
-    
+    <div class="flex flex-col gap-6 w-[400px] mx-auto py-16">
+        <select wire:model="selectContainer " wire:change="changeContinent">
+            <option>Please Select Continent</option>
+            @foreach ($Continent as $item)
+            <option value="{{$item->id}}">{{$item->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="flex-relative">
+        <select wire:model="selectCountry " >
+            <option>Please Select </option>
+            @foreach ($Country as $item)
+            <option value="{{$item->id}}">{{$item->name}}</option>
+            @endforeach
+        </select>
+    </div>
 </div>
