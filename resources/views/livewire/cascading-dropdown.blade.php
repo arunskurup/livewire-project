3157bbe10cbe2a67d7c1ad160b68781e4138b1a8
arunskurup <arunskurup81@gmail.com>
2022-07-24 20:17:28 +0530

    cascad design complete

--- resources/views/livewire/cascading-dropdown.blade.php
+++ resources/views/livewire/cascading-dropdown.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex flex-col gap-6 w-[400px] mx-auto py-16">
         <select wire:model="selectContainer " wire:change="changeContinent">
-            <option>Please Select Continent</option>
+            <option value="-1"> Please Select Continent</option>
             @foreach ($Continent as $item)
             <option value="{{$item->id}}">{{$item->name}}</option>
             @endforeach
@@ -9,7 +9,7 @@
     </div>
     <div class="flex-relative">
         <select wire:model="selectCountry " >
-            <option>Please Select </option>
+            <option value="-1">Please Select </option>
             @foreach ($Country as $item)
             <option value="{{$item->id}}">{{$item->name}}</option>
             @endforeach
