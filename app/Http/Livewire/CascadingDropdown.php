fde96c8a2612c0fddc604879565f68b068f501f9
arunskurup <arunskurup81@gmail.com>
2022-07-24 21:15:11 +0530

    cascad  completed

--- app/Http/Livewire/CascadingDropdown.php
+++ app/Http/Livewire/CascadingDropdown.php
@@ -3,11 +3,36 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use App\Models\Country;
+use App\Models\Continent;
 class CascadingDropdown extends Component
-{
+{   
+    public $Continent = [];
+    public $countries = [];
+
+    public $selectedContinent;
+    public $selectedCountry;
+    
+    //mount
+    public function mount()
+    {
+      $this->Continent = Continent::all();
+    }
+
+   
     public function render()
     {
         return view('livewire.cascading-dropdown');
     }
+
+
+    public function changeContinent()
+    {
+        if ($this->selectedContinent !== '-1'){
+            
+              $this->countries = Country::where('continent_id',$this->selectedContinent)->get();
+        }
+        
+    }
+
 }
--- resources/views/livewire/cascading-dropdown.blade.php
+++ resources/views/livewire/cascading-dropdown.blade.php
@@ -1,18 +1,22 @@
 <div>
     <div class="flex flex-col gap-6 w-[400px] mx-auto py-16">
-        <select wire:model="selectContainer " wire:change="changeContinent">
+        <select wire:model="selectedContinent" wire:change="changeContinent">
             <option value="-1"> Please Select Continent</option>
             @foreach ($Continent as $item)
             <option value="{{$item->id}}">{{$item->name}}</option>
             @endforeach
         </select>
-    </div>
-    <div class="flex-relative">
-        <select wire:model="selectCountry " >
+    
+      <div class="flex relative">
+          <p wire:loading class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-white bg-opacity-90 py-2 px-3">
+            Loading...
+          </p>
+         <select wire:model="selectedCountry" class="flex-1">
             <option value="-1">Please Select </option>
-            @foreach ($Country as $item)
+            @foreach ($countries as $item)
             <option value="{{$item->id}}">{{$item->name}}</option>
             @endforeach
-        </select>
+         </select>
+      </div>
     </div>
 </div>
