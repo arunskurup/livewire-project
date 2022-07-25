688ded4d52809943f84a2d3204a24b3b68e18ace
arunskurup <arunskurup81@gmail.com>
2022-07-25 07:14:18 +0530

    image upload backend Completed

--- app/Http/Livewire/ImageUpload.php
+++ app/Http/Livewire/ImageUpload.php
@@ -8,10 +8,34 @@ use Illuminate\Support\Facades\Storage;
 class ImageUpload extends Component
 {   
     use WithFileUploads;
+    /**
+     * @var \Livewire\TemporaryUploadFile
+     */
 
     public $image = [];
+    
+    public function save()
+    {
+        $this->validate([
+            'image.*' => 'image|max:1024'
+        ]);
+        //store
+        foreach($this->image as $image){
+            $image->store('public');
+        }
+    }
+
     public function render()
     {
-        return view('livewire.image-upload');
+        return view('livewire.image-upload',[
+            'images' => collect(Storage::files('public'))
+            ->fillter(function ($file){
+                return in_array(strtolower(pathinfo($file)),['png','jpg','jpeg','gif','webp']);
+            })
+            ->map(function($file){
+               return Storage::url($file);
+            })
+
+        ]);
     }
 }
