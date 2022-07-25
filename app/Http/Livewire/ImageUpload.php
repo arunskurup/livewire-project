c3712daf652c1523dbebf68958705231ac409f7f
arunskurup <arunskurup81@gmail.com>
2022-07-25 07:00:09 +0530

    image upload1

--- app/Http/Livewire/ImageUpload.php
+++ app/Http/Livewire/ImageUpload.php
@@ -3,9 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 class ImageUpload extends Component
-{
+{   
+    use WithFileUploads;
+
+    public $image = [];
     public function render()
     {
         return view('livewire.image-upload');
