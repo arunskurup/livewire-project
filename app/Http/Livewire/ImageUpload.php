5c778e8976dcacc3f7f8724b45ac31f3726153bb
arunskurup <arunskurup81@gmail.com>
2022-07-25 06:52:57 +0530

    image upload

---
+++ app/Http/Livewire/ImageUpload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ImageUpload extends Component
+{
+    public function render()
+    {
+        return view('livewire.image-upload');
+    }
+}
